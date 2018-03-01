e0f97d84c572a9fa54ab73352a2713972d42e931
LuisErnesto <ernesto.monsalve@udea.edu.co>
2018-02-28 23:31:52 -0500

    Toolbar

--- Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/Dish.java
+++ Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/Dish.java
@@ -2,6 +2,9 @@ package co.edu.udea.compumovil.gr01_20181.labscm20181.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import co.edu.udea.compumovil.gr01_20181.labscm20181.R;
@@ -12,8 +15,27 @@ public class Dish extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_dish);
+        setSupportActionBar(myToolbar);
     }
 
     public void onCheckboxClicked(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_others, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.exit:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/SplashActivity.java
+++ Labs_CM/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr01_20181/labscm20181/views/SplashActivity.java
@@ -18,8 +18,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
 
         Timer RunSplash = new Timer();
         final TimerTask ShowSplash = new TimerTask() {
